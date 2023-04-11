d89ed3cf5ce4d9119aa6d35a86222a4241ce8812
airfishi <36003224+airfishi@users.noreply.github.com>
2023-04-11 16:22:54 +0000

    fixed home url error

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ var sidebar_shopping_list = document.getElementById("shopping_list");
 var ourURL = 'https://airfishi-bug-free-space-lamp-pgwj4rjxwrpf5vq-5000.preview.app.github.dev';
 
 sidebar_pantry_planner.addEventListener("click", function() {
-  window.location.href = ourURL + "/pantry_planner";
+  window.location.href = ourURL + "/";
 });
 
 sidebar_my_food.addEventListener("click", function() {
