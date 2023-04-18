8828198863e6c7ce719d638c2d8842575017a775
airfishi <36003224+airfishi@users.noreply.github.com>
2023-04-18 16:45:15 +0000

    added some more auth0 stuff but not sure if it will work

--- app.py
+++ app.py
@@ -1,5 +1,37 @@
-from flask import Flask, render_template
+from os import environ as env
+
+from authlib.integrations.flask_client import OAuth
+
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, url_for, flash
+
+from components.authentication import create_auth_blueprint
+from components.chat import chat
+
+from helpers.decorators import login_required
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+    
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+# initialize OAuth registry with this fetch_token function
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+app.register_blueprint(create_auth_blueprint(oauth))
+app.register_blueprint(chat)
 
 @app.route('/')
 def index() -> str:
@@ -16,6 +48,7 @@ def pantryplanner() -> str:
     return render_template('homepage.html')
 
 @app.route('/settings')
+@login_required()
 def settings() -> str:
     return render_template('settings.html')
 
@@ -33,7 +66,7 @@ def recipes() -> str:
 
 @app.route('/login')
 def login() -> str:
-    return render_template('login.html')
+    return render_template('welcome.html')
 
 if __name__ == '__main__':
     index()
