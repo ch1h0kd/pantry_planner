9d2c8de72b2e95bc3145c472eeae1cc1ec440751
Chiho Okada <113386829+ch1h0kd@users.noreply.github.com>
2023-04-19 22:59:49 +0000

    copied app.py from main

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template
+from os import environ as env
+
+from authlib.integrations.flask_client import OAuth
+
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, url_for, flash
+
+from components.authentication import create_auth_blueprint
+
+from helpers.decorators import login_required
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+    
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+# initialize OAuth registry with this fetch_token function
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+app.register_blueprint(create_auth_blueprint(oauth))
 
 @app.route('/')
 def index() -> str:
     return render_template('homepage.html')
-
-# I think this is how it would be set up for other pages but not entirely sure -Fish
-# @app.route('/myfood')
-# def myfood() -> str:
-#     return render_template('myfood.html')
 
 # this is a temporary fix hopefully for the button not working
 @app.route('/pantry_planner')
@@ -16,6 +41,7 @@ def pantryplanner() -> str:
     return render_template('homepage.html')
 
 @app.route('/settings')
+@login_required()
 def settings() -> str:
     return render_template('settings.html')
 
@@ -30,6 +56,15 @@ def shopping_list() -> str:
 @app.route('/recipes')
 def recipes() -> str:
     return render_template('recipes.html')
+
+@app.route('/login')
+def login() -> str:
+    return render_template('welcome.html')
+
+@app.route('/logout')
+@login_required()
+def logout() -> str:
+    return render_template('logout.html')
 
 if __name__ == '__main__':
     index()
