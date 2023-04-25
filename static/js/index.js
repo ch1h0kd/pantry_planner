62feb721bafd0cfeb5f8e0147646cc74a750d8c0
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-04-25 19:52:00 +0000

    added in basic username support

--- static/js/index.js
+++ static/js/index.js
@@ -15,10 +15,32 @@ const app = initializeApp({
 });
 
 const db = getDatabase(app);
-const username = "garrett";
-const foodRef = ref(db, username + "/food");
-const shoppingRef = ref(db, username + "/shopping");
+var username = "defualt-user";
+var foodRef = ref(db, username + "/food");
+var shoppingRef = ref(db, username + "/shopping");
+get(shoppingRef).then((snapshot) => {
+  shopHandler(snapshot)
+});
+get(foodRef).then((snapshot) => {
+  foodHandler(snapshot)
+});
 
+export function changeUser(){
+  const userInput = document.getElementById("user-input");
+  const newUser = userInput.value;
+  if(newUser != ""){
+    username = newUser;
+    foodRef = ref(db, username + "/food");
+    shoppingRef = ref(db, username + "/shopping");
+    userInput.value = "";
+  }
+  get(shoppingRef).then((snapshot) => {
+    shopHandler(snapshot)
+  });
+  get(foodRef).then((snapshot) => {
+    foodHandler(snapshot)
+  });
+}
 
 export function addItemExp() {    
     const itemInput = document.getElementById("item-input");
@@ -33,6 +55,9 @@ export function addItemExp() {
         exp: exp
       });
     }
+    get(foodRef).then((snapshot) => {
+      foodHandler(snapshot)
+    });
 }
 
 export function addItemShop() {    
@@ -44,6 +69,9 @@ export function addItemShop() {
       item: item
     });
   }
+  get(shoppingRef).then((snapshot) => {
+    shopHandler(snapshot)
+  });
 }
   
   /*
@@ -54,8 +82,14 @@ export function addItemShop() {
 export function buttonRemove(category, id){
   var toRemove = ref(db, username + category + id); 
   remove(toRemove);
+  get(shoppingRef).then((snapshot) => {
+    shopHandler(snapshot)
+  });
+  get(foodRef).then((snapshot) => {
+    foodHandler(snapshot)
+  });
 }
-  
+/*  
 onValue(foodRef, (snapshot) => { //update the list whenever a new food is added 
   const list = document.getElementById("expList");
   list.innerHTML = "";
@@ -82,7 +116,36 @@ onValue(foodRef, (snapshot) => { //update the list whenever a new food is added
       i++;
   });
 }); 
+*/
 
+function foodHandler(snapshot){
+  const list = document.getElementById("expList");
+  list.innerHTML = "";
+  const trip = Object.values(snapshot.val());
+  const keys = Object.keys(snapshot.val());
+  var i = 0;
+  trip.forEach(element => {        
+      const listItem = document.createElement("li");
+      const itemHeading = document.createElement("h2");
+      var button = document.createElement("button");
+      button.innerHTML = "remove item";
+      button.value = (keys[i]);
+      listItem.appendChild(button);
+      button.style["float"] = "right";
+      button.addEventListener("click", function(){
+        buttonRemove("/food/", button.value);
+      });
+      itemHeading.appendChild(document.createTextNode(element.item));
+      const expPara = document.createElement("p");
+      expPara.appendChild(document.createTextNode(element.exp));
+      listItem.appendChild(itemHeading);
+      listItem.appendChild(expPara);
+      list.appendChild(listItem);
+      i++;
+  });
+}
+
+/*
 onValue(shoppingRef, (snapshot) => { //update the list whenever a new food is added 
   const list = document.getElementById("shopList");
   list.innerHTML = "";
@@ -106,7 +169,32 @@ onValue(shoppingRef, (snapshot) => { //update the list whenever a new food is ad
       i++;
   });
 });
+*/
+function shopHandler(snapshot){
+  const list = document.getElementById("shopList");
+  list.innerHTML = "";
+  const trip = Object.values(snapshot.val());
+  const keys = Object.keys(snapshot.val());
+  var i = 0;
+  trip.forEach(element => {        
+      const listItem = document.createElement("li");
+      const itemHeading = document.createElement("h2");
+      var button = document.createElement("button");
+      button.innerHTML = "remove item";
+      button.value = (keys[i]);
+      listItem.appendChild(button);
+      button.style["float"] = "right";
+      button.addEventListener("click", function(){
+        buttonRemove("/shopping/", button.value);
+      });
+      itemHeading.appendChild(document.createTextNode(element.item));
+      listItem.appendChild(itemHeading);
+      list.appendChild(listItem);
+      i++;
+    });
+}
   
   window.addItemExp = addItemExp; //changes the scope!!! most important line, makes global
   window.addItemShop = addItemShop;
   window.buttonRemove = buttonRemove;
+  window.changeUser = changeUser;
