a22dd44f6abd709bf3904efa4f0bb7004ad61cd7
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-04-25 19:08:41 +0000

    added back the shopping list

--- static/js/index.js
+++ static/js/index.js
@@ -17,6 +17,7 @@ const app = initializeApp({
 const db = getDatabase(app);
 const username = "garrett";
 const foodRef = ref(db, username + "/food");
+const shoppingRef = ref(db, username + "/shopping");
 
 
 export function addItemExp() {    
@@ -26,89 +27,82 @@ export function addItemExp() {
     const exp = expInput.value;
     itemInput.value = "";
     expInput.value = "";
-
-    //implementation that just throws it in the html
-    //const list = document.getElementById("expList");
-    //const listItem = document.createElement("li");
-    //const itemHeading = document.createElement("h2");
-    //itemHeading.appendChild(document.createTextNode(item));
-    //const expPara = document.createElement("p");
-    //expPara.appendChild(document.createTextNode(exp));
-    //listItem.appendChild(itemHeading);
-    //listItem.appendChild(expPara);
-    //list.appendChild(listItem);
-
-    
     push(foodRef, {
         item: item,
         exp: exp
       });
-  }
+}
+
+export function addItemShop() {    
+  const itemInput = document.getElementById("shop-input");
+  const item = itemInput.value;
+  itemInput.value = "";
+  push(shoppingRef, {
+      item: item
+    });
+}
   
   /*
   get(foodRef).then((snapshot) => {
-
-    const trip = Object.values(snapshot.val());
-    const keys = Object.keys(snapshot.val());
-    var i = 0;
-      trip.forEach(element => {    
-        const list = document.getElementById("expList");
-        const listItem = document.createElement("li");
-        const itemHeading = document.createElement("h2");
-        var button = document.createElement("button");
-        button.innerHTML = "remove item";
-        button.value = (keys[i]);
-        listItem.appendChild(button);
-
-        button.addEventListener("click", function(){
-          buttonRemove(button.value);
-        });
-
-        itemHeading.appendChild(document.createTextNode(element.item));
-        const expPara = document.createElement("p");
-        expPara.appendChild(document.createTextNode(element.exp));
-        listItem.appendChild(itemHeading);
-        listItem.appendChild(expPara);
-        list.appendChild(listItem);
-        i++;
-      });
-});
+    in case I need it again the code to get food ref once
 */
 
-export function buttonRemove(id){
-  var toRemove = ref(db, username + "/food/" +id); 
-  console.log(toRemove);
+export function buttonRemove(category, id){
+  var toRemove = ref(db, username + category + id); 
   remove(toRemove);
 }
   
-  onValue(foodRef, (snapshot) => { //update the list whenever a new food is added 
-    const list = document.getElementById("expList");
-    list.innerHTML = "";
-    const trip = Object.values(snapshot.val());
-    const keys = Object.keys(snapshot.val());
-    var i = 0;
-    //const element = trip[trip.length -1];
-      trip.forEach(element => {        
-        const listItem = document.createElement("li");
-        const itemHeading = document.createElement("h2");
-        var button = document.createElement("button");
-        button.innerHTML = "remove item";
-        button.value = (keys[i]);
-        listItem.appendChild(button);
+onValue(foodRef, (snapshot) => { //update the list whenever a new food is added 
+  const list = document.getElementById("expList");
+  list.innerHTML = "";
+  const trip = Object.values(snapshot.val());
+  const keys = Object.keys(snapshot.val());
+  var i = 0;
+  trip.forEach(element => {        
+      const listItem = document.createElement("li");
+      const itemHeading = document.createElement("h2");
+      var button = document.createElement("button");
+      button.innerHTML = "remove item";
+      button.value = (keys[i]);
+      listItem.appendChild(button);
+      button.style["float"] = "right";
+      button.addEventListener("click", function(){
+        buttonRemove("/food/", button.value);
+      });
+      itemHeading.appendChild(document.createTextNode(element.item));
+      const expPara = document.createElement("p");
+      expPara.appendChild(document.createTextNode(element.exp));
+      listItem.appendChild(itemHeading);
+      listItem.appendChild(expPara);
+      list.appendChild(listItem);
+      i++;
+  });
+}); 
 
-        button.addEventListener("click", function(){
-          buttonRemove(button.value);
-        });
-
-        itemHeading.appendChild(document.createTextNode(element.item));
-        const expPara = document.createElement("p");
-        expPara.appendChild(document.createTextNode(element.exp));
-        listItem.appendChild(itemHeading);
-        listItem.appendChild(expPara);
-        list.appendChild(listItem);
-        i++;
-    });
-  }); 
+onValue(shoppingRef, (snapshot) => { //update the list whenever a new food is added 
+  const list = document.getElementById("shopList");
+  list.innerHTML = "";
+  const trip = Object.values(snapshot.val());
+  const keys = Object.keys(snapshot.val());
+  var i = 0;
+  trip.forEach(element => {        
+      const listItem = document.createElement("li");
+      const itemHeading = document.createElement("h2");
+      var button = document.createElement("button");
+      button.innerHTML = "remove item";
+      button.value = (keys[i]);
+      listItem.appendChild(button);
+      button.style["float"] = "right";
+      button.addEventListener("click", function(){
+        buttonRemove("/shopping/", button.value);
+      });
+      itemHeading.appendChild(document.createTextNode(element.item));
+      listItem.appendChild(itemHeading);
+      list.appendChild(listItem);
+      i++;
+  });
+});
   
   window.addItemExp = addItemExp; //changes the scope!!! most important line, makes global
+  window.addItemShop = addItemShop;
   window.buttonRemove = buttonRemove;
