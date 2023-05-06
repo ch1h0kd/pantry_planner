4c508c79b685ac6e2ac3934d8f68859ffa4ba87f
Chiho Okada <113386829+ch1h0kd@users.noreply.github.com>
2023-05-06 03:38:16 +0000

    Search box works

--- static/js/add-food.js
+++ static/js/add-food.js
@@ -20,9 +20,89 @@ var username = "defualt-user";
 var foodRef = ref(db, username + "/food");
 var shoppingRef = ref(db, username + "/shopping");
 get(foodRef).then((snapshot) => {
-  foodHandler(snapshot)
+  //foodHandler(snapshot)
+  initialize(snapshot)
 });
 
+
+function initialize(snapshot){
+  const searchTerm = document.getElementById("searchMyf");
+  // no search has been made yet
+  let lastSearch = '';
+  // finalGroup will contain the products that need to be displayed after the searching has been done.
+  // Set an empty array that is returned later
+  let finalGroup;
+
+  finalGroup = Object.values(snapshot.val()); // show all items in my food as default
+  console.log("final group1 " + finalGroup);
+  console.log("snapshot " + snapshot);
+  showDisplay();
+  
+  console.log("snapshot " + typeof snapshot);
+
+  finalGroup = [];
+
+  document.getElementById("searchMyfB").addEventListener("click", selectItems);
+
+  function selectItems(e){
+    e.preventDefault();
+    // if the category and search term are the same as they were the last time a search was run, the results will be the same
+    if (searchTerm.value.trim() === lastSearch) {
+      return;
+    } else {
+      // update the record of search term
+      lastSearch = searchTerm.value.trim();
+    }
+    
+    // Make sure the search term is converted to lower case before comparison.
+    const lowerCaseSearchTerm = searchTerm.value.trim().toLowerCase();
+    
+    // Filter finalGroup to contain only products whose name includes the search term
+    finalGroup = Object.values(snapshot.val()).filter( element => element.item.includes(lowerCaseSearchTerm));
+    // Once we have the final group, update the display
+    showDisplay();
+  }
+
+  function showDisplay(){
+    const list = document.getElementById("expList");
+    list.innerHTML = "";
+
+    // if no products match the search term, display a "No results" message
+    if (finalGroup.length === 0) {
+      const para = document.createElement('h1');
+      para.textContent = 'No results';
+      list.appendChild(para);
+    }
+
+    const trip = Object.values(finalGroup); //[object Object],[object Object],[object Object]
+    //console.log("Object.values(snapshot.val()) " + trip);
+    const keys = Object.keys(finalGroup); //-NUSIz-AfjvoCJ7y0Htb,-NUSJ16BoyBtw933wrZs,-NUSJ2f6u2gvIT8KgOKc
+    //console.log("Object.keys(snapshot.val()) " + keys);
+    //keys[0]: -NUSIz-AfjvoCJ7y0Htb
+    var i = 0;
+    trip.forEach(element => {
+        //element.item: beans
+        //element.exp: 2023-05-31
+        const listItem = document.createElement("section");
+        const itemHeading = document.createElement("h3");
+        var button = document.createElement("button");
+        button.innerHTML = "remove item";
+        button.value = (keys[i]);
+        listItem.appendChild(button);
+        button.style["float"] = "right";
+        button.addEventListener("click", function(){
+          buttonRemove("/food/", button.value);
+        });
+        itemHeading.appendChild(document.createTextNode(element.item));
+        const expPara = document.createElement("p");
+        expPara.appendChild(document.createTextNode(element.exp));
+        listItem.appendChild(itemHeading);
+        listItem.appendChild(expPara);
+        list.appendChild(listItem);
+        i++;
+    });
+  }
+}
 
 export function changeUser(){
   const userInput = document.getElementById("user-input-pop");
@@ -38,22 +118,22 @@ export function changeUser(){
     userInput.value = "";
   }
   get(foodRef).then((snapshot) => {
-    foodHandler(snapshot)
+    //foodHandler(snapshot)
+    initialize(snapshot)
   });
 }
 
 export function addItemExp() {   
-    console.log("addItemExp"); 
     const itemInput = document.getElementById("item-input-pop");
     const expInput = document.getElementById("exp-input-pop");
     const tagInput = document.querySelector("#tag");
     const item = itemInput.value;
     const exp = expInput.value;
-    console.log(tagInput);
+    //console.log(tagInput);
     if(item != "" && exp != "" && tag != ""){
       itemInput.value = "";
       expInput.value = "";
-      tag.value = "";
+      tagInput.value = ""; //modify the rule on firebase
       push(foodRef, {
         item: item,
         exp: exp,
@@ -61,7 +141,8 @@ export function addItemExp() {
       });
     }
     get(foodRef).then((snapshot) => {
-      foodHandler(snapshot)
+      //foodHandler(snapshot)
+      initialize(snapshot)
     });
 }
 
@@ -87,36 +168,10 @@ export function buttonRemove(category, id){
     shopHandler(snapshot)
   });
   get(foodRef).then((snapshot) => {
-    foodHandler(snapshot)
+    //foodHandler(snapshot)
+    initialize(snapshot)
   });
 }
 
-function foodHandler(snapshot){
-  const list = document.getElementById("expList");
-  list.innerHTML = "";
-  const trip = Object.values(snapshot.val());
-  const keys = Object.keys(snapshot.val());
-  var i = 0;
-  trip.forEach(element => {        
-      const listItem = document.createElement("section");
-      const itemHeading = document.createElement("h3");
-      var button = document.createElement("button");
-      button.innerHTML = "remove item";
-      button.value = (keys[i]);
-      listItem.appendChild(button);
-      button.style["float"] = "right";
-      button.addEventListener("click", function(){
-        buttonRemove("/food/", button.value);
-      });
-      itemHeading.appendChild(document.createTextNode(element.item));
-      const expPara = document.createElement("p");
-      expPara.appendChild(document.createTextNode(element.exp));
-      listItem.appendChild(itemHeading);
-      listItem.appendChild(expPara);
-      list.appendChild(listItem);
-      i++;
-  });
-}
-  
 window.addItemExp = addItemExp; //changes the scope!!! most important line, makes global
 window.changeUser = changeUser;
