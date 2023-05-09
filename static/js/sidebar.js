dc982199b23da3e131dfa52e2cb036eaf1e3c44c
airfishi <36003224+airfishi@users.noreply.github.com>
2023-05-09 17:08:51 +0000

    changed login stuff

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,7 +3,7 @@ var sidebar_my_food = document.getElementById("my_food");
 var sidebar_recipes = document.getElementById("recipes");
 var sidebar_settings = document.getElementById("settings");
 var sidebar_shopping_list = document.getElementById("shopping_list");
-var sidebar_shopping_list = document.getElementById("login");
+var login = document.getElementById("login");
 //need to fix the url here for other people to be able to use the login button
 
 sidebar_pantry_planner.addEventListener("click", function() {
@@ -11,11 +11,11 @@ sidebar_pantry_planner.addEventListener("click", function() {
 });
 
 sidebar_my_food.addEventListener("click", function() {
-    window.location.href = "www.cooklog.nl" + "/my_food"
+  window.location.href = "www.cooklog.nl" + "/my_food"
 });
 
 sidebar_recipes.addEventListener("click", function() {
-    window.location.href = "www.cooklog.nl" + "/recipes"
+  window.location.href = "www.cooklog.nl" + "/recipes"
 });
 
 sidebar_settings.addEventListener("click", function() {
@@ -27,5 +27,5 @@ sidebar_shopping_list.addEventListener("click", function() {
 });
 
 login.addEventListener("click", function() {
-  window.location.href = "www.cooklog.nl" + "/login";
+  window.location.href = "/login";
 });
