7a354e05541ca419dcc8fac7c9b363e9a2304a0b
airfishi <36003224+airfishi@users.noreply.github.com>
2023-05-11 15:19:20 +0000

    reverted to old nickname

--- static/js/index.js
+++ static/js/index.js
@@ -15,7 +15,7 @@ const app = initializeApp({
 });
 
 const db = getDatabase(app);
-var username = 'baseline'
+var username = '{{ nickname }}'
 // var username = fetch('/getnickname').then(response => console.log(response));
 var foodRef = ref(db, username + "/food");
 var shoppingRef = ref(db, username + "/shopping");
