4167baf512fc93cda9def7865aa02c7bc1ee9351
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-05-12 22:52:12 +0000

    weikno

--- static/js/index.js
+++ static/js/index.js
@@ -15,8 +15,9 @@ const app = initializeApp({
 });
 
 const db = getDatabase(app);
-var username = '{{ nickname }}'
-//var username = fetch('/getnickname').then(response => console.log(response));
+//var username = '{{ nickname }}'
+var username = fetch('/getnickname').then(response => console.log(response));
+console.log(username);
 var foodRef = ref(db, username + "/food");
 var shoppingRef = ref(db, username + "/shopping");
 get(foodRef).then((snapshot) => {
