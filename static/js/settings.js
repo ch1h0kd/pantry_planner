2a993a8948312425a0e6e556e81441a10373281d
airfishi <36003224+airfishi@users.noreply.github.com>
2023-05-23 15:47:46 +0000

    fixing url

--- static/js/settings.js
+++ static/js/settings.js
@@ -2,6 +2,6 @@ const lgb = document.getElementById("logout_button");
 
 if (lgb){
 lgb.addEventListener("click", function() {
-    window.location.href = "www.cooklog.nl" + "/logout";
+    window.location.href = "/logout";
   }, false);
 }
